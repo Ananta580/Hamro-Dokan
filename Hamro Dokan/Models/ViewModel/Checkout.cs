5210fda4aa1cd4c1236708c4fd132e1cb0973093
Ananta580 <45358510+Ananta580@users.noreply.github.com>
2019-09-28 21:54:21 +0545

    Added Files

---
+++ Hamro Dokan/Models/ViewModel/Checkout.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hamro_dokan.Models.ViewModel
+{
+    public class Checkout
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int ZipCode { get; set; }
+        public int Telephone { get; set; }
+
+
+    }
+}
