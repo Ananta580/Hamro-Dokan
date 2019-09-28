1515f841936efac26c7325c837cfcf26ee650be3
Ananta580 <45358510+Ananta580@users.noreply.github.com>
2019-09-28 21:48:09 +0545

    Add files via upload

---
+++ Hamro Dokan/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hamro_dokan.MvcApplication" Language="C#" %>
---
+++ Hamro Dokan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hamro_dokan.Startup))]
+namespace Hamro_dokan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
